53034a5e313fad06cad7199df13f29e6ded373ed
Gopichandar <idartsindia9099@outlook.com>
2021-10-21 18:30:55 +0530

    Results added as comment

--- FastStringCreation/Program.cs
+++ FastStringCreation/Program.cs
@@ -10,6 +10,14 @@ namespace FastStringCreation
         static void Main(string[] args)
         {
             BenchmarkRunner.Run<Benchy>();
+            /*
+            | Method                | Mean      | Error    | StdDev   | Median    | Gen 0  | Allocated |
+            | --------------------- | ---------:| --------:| --------:| ---------:| ------:| ---------:|
+            | MaskNaive             | 193.44 ns | 3.951 ns | 4.704 ns | 194.75 ns | 0.1273 | 400 B     |
+            | MaskUsingStringBuilder| 74.79 ns  | 0.715 ns | 0.669 ns | 74.84 ns  | 0.0587 | 184 B     |
+            | MaskNewString         | 38.78 ns  | 0.815 ns | 2.176 ns | 37.92 ns  | 0.0382 | 120 B     |
+            | MaskStringCreate      | 18.26 ns  | 0.431 ns | 0.618 ns | 18.03 ns  | 0.0153 | 48 B      |
+            */
             Console.ReadKey();
         }
     }
